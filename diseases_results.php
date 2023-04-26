84c9c7c9b65df26dc888b516e8394652fe67edd9
shivangigaur15 <shivangig@iitbhilai.ac.in>
2023-04-26 23:58:12 +0530

    diseases_results linked to remedies

--- diseases_results.php
+++ diseases_results.php
@@ -1,5 +1,6 @@
 <html>
 <body>
+
 <?php
 $diseases = array(
    "Common cold" => array("Fever", "Headache", "Cough", "Sore throat", "Fatigue", "Body aches/pain"),
@@ -27,11 +28,23 @@ foreach ($diseases as  $disease => $disease_symptoms)
 {
 	if( count(array_intersect($symptoms,$disease_symptoms)) == $max )
 	{
+		$final_disease=$disease;
 		echo "<h2>Possible disease: " . $disease . "</h2>";
     	break;
     	}
 }
 ?>
+
+
+<p>
+Do you want to know about remedies, food plans and medicines you can take in this disease?
+
+<form action="remedies.php" method="get">
+<input type="hidden" name="finalDisease" value="<?php echo "$final_disease" ?>">
+<input type="submit" name="remedy" value="Click here">
+</form>
+</p>
+
 </body>
 </html>
 
