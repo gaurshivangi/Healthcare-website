6a9cf240fdbbf2e691455d6c06716befdc6c3191
shivangigaur15 <shivangig@iitbhilai.ac.in>
2023-04-26 12:53:45 +0530

    remedy upload only text no code

--- remedies.php
+++ remedies.php
@@ -1,36 +1,30 @@
 <html>
 <body>
 <?php
-$diseases = array(
-   "Common cold" => array("Fever", "Headache", "Cough", "Sore throat", "Fatigue", "Body aches/pain"),
-   "Pneumonia" => array("Fever", "Cough", "Chest pain", "Shortness of breath", "Fatigue"),
-   "Influenza" => array("Fever", "Headache", "Cough", "Fatigue", "Body aches/pain", "Nausea/Vomiting", "Diarrhea"),
-   "Allergies" => array("Headache", "Sore throat", "Itching", "Skin rash"),
-   "Anxiety" => array("Dizziness", "Lightheadedness")
+$home_remedies = array(
+   "Common cold" => "Drink warm fluids, rest, use a humidifier, gargle with saltwater",
+   "Pneumonia" => "Get plenty of rest, drink fluids, use a humidifier, take over-the-counter pain relievers",
+   "Influenza" => "Get plenty of rest, drink fluids, take over-the-counter pain relievers, use a humidifier",
+   "Allergies" => "Avoid allergens, use over-the-counter antihistamines, use nasal irrigation",
+   "Anxiety" => "Practice relaxation techniques, exercise regularly, get enough sleep"
 );
 
-$symptoms = $_POST['symptoms'];
+$food_plans = array(
+   "Common cold" => "Drink warm fluids, eat foods high in vitamin C and zinc, avoid dairy products",
+   "Pneumonia" => "Drink fluids, eat foods high in vitamin C and zinc, avoid dairy products",
+   "Influenza" => "Drink fluids, eat foods high in vitamin C and zinc, avoid dairy products",
+   "Allergies" => "Eat foods high in vitamin C and omega-3 fatty acids, avoid processed foods and sugar",
+   "Anxiety" => "Eat foods rich in omega-3 fatty acids, complex carbohydrates, and magnesium"
+);
 
-$max=0;
-$matches=0;
-foreach ($diseases as  $disease => $disease_symptoms) 
-{
-	$matches=count(array_intersect($symptoms,$disease_symptoms));
-   if ( $matches > $max ) 
-   {
-      $max=$matches;
-   }
-   
-}
+$otc_medicines = array(
+   "Common cold" => "Acetaminophen, ibuprofen, decongestants, cough suppressants",
+   "Pneumonia" => "Antibiotics, acetaminophen, ibuprofen, decongestants, cough suppressants",
+   "Influenza" => "Acetaminophen, ibuprofen, decongestants, cough suppressants, antiviral medication",
+   "Allergies" => "Antihistamines, decongestants, nasal corticosteroids, allergy shots",
+   "Anxiety" => "Antidepressants, anti-anxiety medication"
+);
 
-foreach ($diseases as  $disease => $disease_symptoms)
-{
-	if( count(array_intersect($symptoms,$disease_symptoms)) == $max )
-	{
-		echo "<h2>Possible disease: " . $disease . "</h2>";
-    	break;
-    	}
-}
 ?>
 </body>
 </html>
