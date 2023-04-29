0d1c4318ce427b94dd6dc2145143d6cd16b0653b
shivangigaur15 <shivangig@iitbhilai.ac.in>
2023-04-29 21:28:36 +0530

    login page

---
+++ search.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="tosearch.php" method="post">
+<input  name="email"/>E-mail<br>
+<input type="password" name="password"/>Password<br>
+<input type="submit" name="submit" value="submit"> <br><br>
+
+</form>
+
+<a href="signup_final.php">Sign Up</a>
+</body>
+</html>
+
